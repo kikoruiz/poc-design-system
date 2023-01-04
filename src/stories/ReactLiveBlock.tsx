10e0c2b5a4fa0af8a7b61566a1ab6b2287cb5d5d
Powerplex <julien.bonnin.dev@gmail.com>
2023-01-04 13:21:36 +0100

    improved editable jsx examples

--- src/stories/ReactLiveBlock.tsx
+++ src/stories/ReactLiveBlock.tsx
@@ -1,19 +1,33 @@
+import React, { ReactElement, useState } from "react";
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import reactElementToJSXString from "react-element-to-jsx-string";
+
 import { useClipboard } from "./useClipboard";
 
-import { useState } from "react";
-
-interface Props {
-  code: string;
+interface ReactLiveBlockProps {
+  children: ReactElement | ReactElement[];
   scope?: Record<string, any>;
   editable?: boolean;
 }
 
-export function ReactLiveBlock({ code, editable = true, scope }: Props) {
-  const [editorCode, setEditorCode] = useState(code.trim());
+export function ReactLiveBlock({
+  children,
+  editable = true,
+  scope,
+}: ReactLiveBlockProps) {
+  const childrenLength = React.Children.toArray(children).length;
+  const code = reactElementToJSXString(
+    childrenLength > 1 ? <>{children}</> : children,
+    {
+      showFunctions: true,
+      maxInlineAttributesLineLength: 150,
+    }
+  );
+
+  const [editorCode, setEditorCode] = useState(code);
   const { hasCopied, onCopy } = useClipboard(editorCode);
 
-  const onChange = (newCode: string) => setEditorCode(newCode.trim());
+  const onChange = (newCode: string) => setEditorCode(newCode);
   const reset = () => setEditorCode(code);
 
   return (
@@ -56,6 +70,54 @@ export function ReactLiveBlock({ code, editable = true, scope }: Props) {
         <LiveEditor disabled={!editable} onChange={onChange} />
       </div>
       {editable && <LiveError />}
+    </LiveProvider>
+  );
+}
+
+interface CodeSnippetProps {
+  code: string;
+}
+
+export function CodeSnippet({ code }: CodeSnippetProps) {
+  const { hasCopied, onCopy } = useClipboard(code);
+
+  return (
+    <LiveProvider code={code}>
+      <LivePreview />
+      <div
+        style={{
+          position: "relative",
+          borderRadius: "10px",
+          padding: "10px",
+          marginTop: "10px",
+          outline: "none",
+          background: "#333",
+        }}
+      >
+        <p
+          style={{
+            textAlign: "center",
+            color: "white",
+            margin: 0,
+            fontWeight: 700,
+          }}
+        >
+          Code snippet
+        </p>
+
+        <div
+          style={{
+            position: "absolute",
+            display: "flex",
+            gap: 5,
+            top: 5,
+            right: 5,
+          }}
+        >
+          <button onClick={onCopy}>{hasCopied ? "Copied" : "Copy"}</button>
+        </div>
+        <LiveEditor disabled />
+      </div>
     </LiveProvider>
   );
 }
