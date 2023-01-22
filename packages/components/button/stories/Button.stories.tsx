63453c3588bbbda6106ab86819c46aca7cedf9a6
andresin87 <andresin87@gmail.com>
2023-01-22 23:19:06 +0100

    add story files removed.

---
+++ packages/components/button/stories/Button.stories.tsx
@@ -0,0 +1,23 @@
+import { Stack } from '@components/stack'
+import { ReactLiveBlock } from '@devtools/ReactLiveBlock'
+
+import { Button } from '../src/'
+
+export const Variants = () => (
+  <ReactLiveBlock scope={{ Button, Stack }}>
+    <Stack alignItems="end">
+      <Button intent="primary">primary button</Button>
+      <Button intent="secondary">secondary button</Button>
+    </Stack>
+  </ReactLiveBlock>
+)
+
+export const Sizes = () => (
+  <ReactLiveBlock scope={{ Button, Stack }}>
+    <Stack alignItems="end">
+      <Button size="small">small button</Button>
+      <Button size="medium">medium button</Button>
+      <Button size="large">large button</Button>
+    </Stack>
+  </ReactLiveBlock>
+)
