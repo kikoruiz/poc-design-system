8d43e2ea2ae95d84cee42dbe3c5b5c65a0a021c2
Kiko Ruiz Lloret <kikoruizlloret@gmail.com>
2023-01-27 10:56:48 +0100

    initial commit

--- packages/components/button/src/Button.variants.ts
+++ packages/components/button/src/Button.variants.ts
@@ -1,43 +1,32 @@
 import { cva, VariantProps } from 'class-variance-authority'
 
-export const buttonVariants = cva(
-  [
-    'font-semibold',
-    'border-xs',
-    'border-solid',
-    'rounded-xs',
-    'hover:transition-colors',
-    'hover:duration-100',
-    'border-transparent',
-  ],
-  {
-    variants: {
-      disabled: {
-        true: ['opacity-50', 'cursor-not-allowed'],
-      },
-      intent: {
-        primary: ['bg-bg-primary', 'text-fg-cta', 'hover:bg-bg-primary-subtle'],
-        secondary: ['bg-bg-secondary', 'text-main-text', 'hover:bg-bg-secondary-subtle'],
-      },
-      size: {
-        small: ['text-s', 'px-s', 'py-xs'],
-        medium: ['text-m', 'px-m', 'py-s'],
-        large: ['text-l', 'px-l', 'py-m'],
-      },
+export const buttonVariants = cva(['spark-Button'], {
+  variants: {
+    disabled: {
+      true: ['opacity-50', 'cursor-not-allowed'],
     },
-    defaultVariants: {
+    intent: {
+      primary: ['bg-bg-primary', 'text-fg-cta', 'hover:bg-bg-primary-subtle'],
+      secondary: ['bg-bg-secondary', 'text-main-text', 'hover:bg-bg-secondary-subtle'],
+    },
+    size: {
+      small: ['text-s', 'px-s', 'py-xs'],
+      medium: ['text-m', 'px-m', 'py-s'],
+      large: ['text-l', 'px-l', 'py-m'],
+    },
+  },
+  defaultVariants: {
+    intent: 'primary',
+    size: 'small',
+  },
+  compoundVariants: [
+    {
       intent: 'primary',
       size: 'small',
+      disabled: true,
+      className: 'border-4 border-pink-500',
     },
-    compoundVariants: [
-      {
-        intent: 'primary',
-        size: 'small',
-        disabled: true,
-        className: 'border-4 border-pink-500',
-      },
-    ],
-  }
-)
+  ],
+})
 
 export type ButtonVariantsProps = VariantProps<typeof buttonVariants> // & { intent: string } <- to mark a prop as required
