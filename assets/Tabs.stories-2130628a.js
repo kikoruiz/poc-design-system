27c3df1e9fc6a4c8d16227edbe67843bd96de395
arnau-rius <32937662+arnau-rius@users.noreply.github.com>
2023-01-31 09:55:13 +0000

    Deploying to gh-pages from @ kikoruiz/poc-design-system@88e87c60d5d91875272c9df41bb56c7ae630044f 🚀

---
+++ assets/Tabs.stories-2130628a.js
@@ -0,0 +1,15 @@
+import"./chunk-MA2MUXQN-dfcc83d7.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{M as c,A as l,S as d}from"./index-0a62f3d6.js";import"./index-557fd086.js";import{S as o}from"./index-56f92e4a.js";import{V as p,T as e}from"./Tabs.stories-7272c2f3.js";import{j as t,a as b,F as g}from"./jsx-runtime-a9c13c85.js";import{u as m}from"./index-f3daabb9.js";import"./preload-helper-41c905a7.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-ffd4619b.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-466c67b7.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-8f1b41fb.js";import"./chunk-XHUUYXNA-0d7bec29.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-BVZGY62N-610dc239.js";import"./_getTag-6acf5a83.js";import"./_baseIsEqual-0821e666.js";import"./inheritsLoose-321c38e7.js";import"./index-639f2073.js";import"./index-5a3d8e9d.js";import"./index.esm-6031b14f.js";function T(s={}){const{wrapper:a}=Object.assign({},m(),s.components);return a?t(a,{...s,children:t(i,{})}):i();function i(){const n=Object.assign({h1:"h1",p:"p",pre:"pre",code:"code"},m(),s.components);return b(g,{children:[t(c,{title:"Components/Disclosure/Tabs",component:e,subcomponents:{"Tabs.List":e.List,"Tabs.Trigger":e.Trigger,"Tabs.Content":e.Content}}),`
+`,t(n.h1,{children:"Tabs"}),`
+`,t(n.p,{children:"A set of layered sections of content—known as tab panels—that are displayed one at a time."}),`
+`,t(o,{label:"Installation"}),`
+`,t(n.pre,{children:t(n.code,{children:`npm install @ds-name/tabs
+`})}),`
+`,t(o,{label:"Usage"}),`
+`,t(n.p,{children:"Import all parts and piece them together."}),`
+`,t(n.pre,{children:t(n.code,{children:`import { Tabs } from "@ds-name/tabs";
+`})}),`
+`,t(o,{label:"Props"}),`
+`,t(l,{of:e}),`
+`,t(o,{label:"Variants"}),`
+`,t(d,{name:"variants",story:p})]})}}const h=p;h.storyName="variants";const r={title:"Components/Disclosure/Tabs",component:e,subcomponents:{"Tabs.List":e.List,"Tabs.Trigger":e.Trigger,"Tabs.Content":e.Content},tags:["stories-mdx"],includeStories:["_Variants_"]};r.parameters=r.parameters||{};r.parameters.docs={...r.parameters.docs||{},page:T};const B=["_Variants_"];export{h as _Variants_,B as __namedExportsOrder,r as default};
+//# sourceMappingURL=Tabs.stories-2130628a.js.map
